30ab1885375828b5b88a892e880ba6d8787c1cd1
Tarek Samaali <tarek.samaali.92@gmail.com>
2018-04-04 18:11:30 +0200

    Update CNN_Draft_ForwardPropagation.c

--- CNN_Draft_ForwardPropagation.c
+++ CNN_Draft_ForwardPropagation.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
+
 #define UPPER_BOUND 10.0
 #define GEN_RANDOM_SEED srand(time(NULL))
 #define DEBUG printf("debug !")
-
+#define ERROR_DIMENSION printf("Dimension conflict: Please review the dimensions of the convolved arrays!")
+#define ERROR_DEPTH printf("Cannot perform convolution: Please make sure the kernel and the block have the same depth.")
 
 typedef struct {
     int depth;
@@ -160,6 +163,67 @@ Block* AddPadding(Block** block,int padding){
 
 }
 
+float convolve_multiplication_sum(Block* block1, Block* block2){
+    int depth,width,height;
+    if(block1->depth!=block2->depth || block1->depth!=block2->depth ||\
+                            block1->depth!=block2->depth){
+                                ERROR_DIMENSION;
+                                exit(0);
+                            }
+    else{
+        float output=0;
+        for(depth=0;depth<block1->depth;depth++){
+            for(height=0;height<block1->height;height++){
+                for(width=0;width<block1->width;width++){
+                    output+=block1->matrix[depth][height][width]\
+                        *block2->matrix[depth][height][width];
+            }
+        }
+    }
+
+        return output;
+    }
+}
+
+float ** convolve(Block* block, Block* kernel, int stride, int padding){
+
+    if(block->depth!=kernel->depth){
+
+        ERROR_DEPTH;
+        exit(0);
+    }else{
+
+    int height=block->height;
+
+    AddPadding(&block,padding);
+    int size_output=(int)(floor((height-kernel->height+2*padding)/stride))+1;
+    float output_convolution[size_output][size_output];
+
+    int size_half_kernel=(int)((kernel->height-1)/2);
+    int begin_point_height=size_half_kernel;
+    int end_point_height=block->height-begin_point_height+1;
+
+    int begin_point_width=begin_point_height;
+    int end_point_width=block->width-begin_point_width+1;
+
+    int index_height_output;
+    int index_width_output;
+
+    for(index_height_output=begin_point_height;index_height_output<end_point_height;index_height_output++){
+        for(index_width_output=begin_point_width;index_width_output<size_output;begin_point_width++){
+            Block* extracted_block=Extract_From_Block(block,0,kernel->depth,index_height_output-size_half_kernel,\
+                                                      index_height_output+size_half_kernel,index_width_output-size_half_kernel,\
+                                                      index_width_output+size_half_kernel);
+            output_convolution[index_height_output-begin_point_height][index_width_output-begin_point_width]=\
+                                convolve_multiplication_sum(extracted_block,kernel);
+        }
+    }
+
+    return output_convolution;
+   }
+
+}
+
 
 
 void display_Block(Block* grid){
@@ -187,8 +251,10 @@ int main()
     Block* layer;
     Block* layer1;
     create_Block(&layer,3,10,5,"random");
-    display_Block(layer);
+    create_Block(&layer1,3,2,2,"random");
 
+    float **convolved;
+    cnvolved=convolve(layer,layer1,1,1);
     /************ Extra *********/
     /*
     layer1=Extract_From_Block(layer,0,3,0,3,0,3);
@@ -197,4 +263,5 @@ int main()
     */
 
     return 0;
+    
 }
